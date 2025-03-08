c54cb4f42b2afa48f7048e14f2a83e2b6d7e09d7
Minh Quan <minhquan10tin@gmail.com>
2025-03-08 11:03:41 +0700

    temp fix(1)

--- src/app/(website)/post/[slug]/page.tsx
+++ src/app/(website)/post/[slug]/page.tsx
@@ -3,13 +3,15 @@ import { client } from "@/sanity/lib/client";
 import React from "react";
 import { CalendarIcon } from "lucide-react";
 
+// Define the type explicitly
 interface PageProps {
   params: {
     slug: string;
   };
 }
 
-async function getPost(slug: string): Promise<SanityTypes.Post> {
+// Fetch post from Sanity
+async function getPost(slug: string): Promise<SanityTypes.Post | null> {
   const query = `*[_type=='post' && slug.current == $slug][0]{
       _id,
       publishedAt,
@@ -22,11 +24,17 @@ async function getPost(slug: string): Promise<SanityTypes.Post> {
       slug,
       body
   }`;
+
   return await client.fetch(query, { slug });
 }
 
+// Main page component
 export default async function Post({ params }: PageProps) {
   const post = await getPost(params.slug);
+
+  if (!post) {
+    return <div className="text-center">Post not found</div>;
+  }
 
   return (
     <div className="flex flex-col items-center w-full">
