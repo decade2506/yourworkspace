87beaf82c9b5b12960968224d60a161b3832d169
Minh Quan <minhquan10tin@gmail.com>
2025-03-12 13:53:20 +0700

    test post title fix

--- src/app/(website)/post/[slug]/page.tsx
+++ src/app/(website)/post/[slug]/page.tsx
@@ -97,10 +97,10 @@ export default async function Post(props: any) {
                     height={200}
                     width={200}
                     quality={100}
-                    className="rounded-2xl min-w-[80px] md:max-w-[80px] lg:max-w-[100px] aspect-square object-cover object-center cursor-pointer"
+                    className="rounded-2xl xs:min-w-[70px] min-w-[80px] md:max-w-[80px] lg:max-w-[100px] aspect-square object-cover object-center cursor-pointer"
                   />
                 </Link>
-                <div className="flex flex-col gap-2 items-starts xs:max-w-[80px] sm:max-w-[180px] md:max-w-[130px] lg:max-w-[150px] xl:max-w-[100px]">
+                <div className="flex flex-col gap-2 items-starts xs:max-w-[100px] sm:max-w-[180px] md:max-w-[130px] lg:max-w-[150px] xl:max-w-[100px]">
                   <div>
                     <Link
                       href={`/post/${latestPosts.slug.current}`}
